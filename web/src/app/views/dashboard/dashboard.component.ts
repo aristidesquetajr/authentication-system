1e5c942e1ff42c550c74e0c291a731c797821c21
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-09-14 11:28:14 +0100

    feat(web): added bootstrap and change contactos for products

--- web/src/app/views/dashboard/dashboard.component.ts
+++ web/src/app/views/dashboard/dashboard.component.ts
@@ -1,33 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Contacto } from 'src/app/models/Contacto';
 import { Permission } from 'src/app/models/Permission';
+import { Product } from 'src/app/models/Product';
 import { getPermissions } from 'src/app/utils/localStorage';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  private permissions: Permission[] = [];
+  hasCreatePermission = false;
+  hasDeletePermission = false
+  hasEditPermission = false;
 
-  private permissions: Permission[] = []
-  hasCreatePermission = false
-
-  contactos: Contacto[] = [
-    new Contacto({
-      name: "Jorge",
-      phone: "945766707",
-      avatarUrl: "http://localhost:4200/assets/contactos-avatar/Foto.png"
-    }),
-    new Contacto({
-      name: "Ana Paula",
-      phone: "949305974",
-      avatarUrl: "http://localhost:4200/assets/contactos-avatar/Foto-1.png"
-    })
-  ]
+  products: Product[] = [
+    {
+      id: Math.random().toString(36),
+      name: 'Cadeira',
+      price: 435,
+    },
+  ];
 
   ngOnInit(): void {
-    this.permissions = getPermissions()
-    this.hasCreatePermission = this.permissions[0].checked
+    this.permissions = getPermissions();
+    this.hasCreatePermission = this.permissions[0].checked;
+    this.hasDeletePermission = this.permissions[1].checked;
+    this.hasEditPermission = this.permissions[2].checked;
   }
 }
