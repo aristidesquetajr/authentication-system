18a699f4e1f8949bffb26c39f43e7a2e8ec3d312
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-09-14 12:34:11 +0100

    feat(web): added method for login

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { PermissionsComponent } from './views/permissions/permissions.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/views/login/login.component.ts
+++ web/src/app/views/login/login.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/User';
+import { getUser } from 'src/app/utils/localStorage';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  private user = {} as User;
+
+  formLogin: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.formLogin = this.fb.group({
+      email: new FormControl('', Validators.required),
+      pass: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    this.user = getUser();
+  }
+
+  signIn() {
+    if (this.login(this.formLogin.value)) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      Swal.fire('email ou senha errada', '', 'error');
+    }
+  }
+
+  login({ email, pass }: User) {
+    if (email !== this.user.email) return;
+
+    if (pass !== this.user.pass) return;
+
+    return true;
+  }
+}
